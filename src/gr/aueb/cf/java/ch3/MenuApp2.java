b1abc3575d55ed0ad9002150251838e632a41ee1
Lampros Vasilakos <166054264+LamprosVasilakos@users.noreply.github.com>
2025-04-09 20:31:40 +0300

    04/09/2025 lesson added

---
+++ src/gr/aueb/cf/java/ch3/MenuApp2.java
@@ -0,0 +1,26 @@
+package gr.aueb.cf.java.ch3;
+
+import java.util.Scanner;
+
+/**
+ * Εκφράζει τη λογική της do-while αλλά με
+ * τη χρήση αέναου while-do και sentinel.
+ */
+public class MenuApp2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int EXIT = 3;
+        int choice;
+
+        while (true) {
+            System.out.println("Παρακαλώ επιλέξτε ένα από τα παρακάτω:");
+            System.out.println("1. Εισαγωγή Προϊόντος");
+            System.out.println("2. Διαγραφή προϊόντος");
+            System.out.println("3. Έξοδος");
+            choice = scanner.nextInt();
+            if (choice == EXIT) break;
+        }
+        System.out.println("Ευχαριστούμε για τη χρήση της εφαρμογής!!");
+    }
+}
