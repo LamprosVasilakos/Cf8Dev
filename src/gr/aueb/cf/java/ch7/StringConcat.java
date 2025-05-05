e450e0e517386a3a1f174f4f079eb6b641e3dc7e
Lampros Vasilakos <166054264+LamprosVasilakos@users.noreply.github.com>
2025-05-06 02:15:06 +0300

    05/05/2025 lesson added

---
+++ src/gr/aueb/cf/java/ch7/StringConcat.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.java.ch7;
+
+
+/**
+ * Συνενώνει δύο ή περισσότερα Strings.
+ */
+public class StringConcat {
+
+    public static void main(String[] args) {
+        String s1 = "Coding";
+        String s2 = "Factory";
+
+        String s3 = s1 + s2;    // + operator in Java is overloaded
+        String s4 = s1.concat(s2);
+
+        String s5 = s1 + " " + s2;
+        String s6 = s1.concat(" ").concat(s2);  // chaining
+
+
+        System.out.println("*".repeat(10));
+    }
+}
---
+++ src/gr/aueb/cf/java/ch7/StringCopy.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.java.ch7;
+
+
+/**
+ * Κανονική αντιγραφή με αντιγραφή δεικτών
+ * γιατί τα Strings είναι immutable.
+ */
+public class StringCopy {
+
+    public static void main(String[] args) {
+        String s1 = "Coding";
+        String s2 = s1; // Copy
+
+        s2 = "Hello";
+    }
+}
+
+
